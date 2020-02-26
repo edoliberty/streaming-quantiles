ad7bcf1029e73e9db9ada655400322e693e31732
Pavel Vesely <vesely@iuuk.mff.cuni.cz>
2020-02-26 14:07:25 +0000

    rel. error sketch improvements (measuring max. error, better arguments)

--- relativeErrorSketch.py
+++ relativeErrorSketch.py
@@ -10,8 +10,8 @@ from math import ceil
 from numpy.random import random, geometric
 
 class RelativeErrorSketch:
-    def __init__(self, eps=0.01, schedule='deterministic', initMaxSize=0, lazy=True, alternate=True): #, bounded = False -- PV: makes sense only with schedule == 'alwaysAll', o/w sketch size must grow with new items
-        eps_lower_bound, eps_upper_bound = 1e-6, 0.5
+    def __init__(self, eps=0.01, schedule='deterministic', always=-1, never=-1, sectionSize=-1, initMaxSize=0, lazy=True, alternate=True):
+        eps_lower_bound, eps_upper_bound = 1e-6, 0.5 #TODO remove?
         if eps < eps_lower_bound or eps > eps_upper_bound:
             raise ValueError(f"eps must be int the range [{eps_lower_bound}.{eps_upper_bound}]")
         self.eps = eps
@@ -21,16 +21,22 @@ class RelativeErrorSketch:
         self.lazy = lazy
         self.alternate = alternate
         self.schedule = schedule
-        #self.bounded = bounded
-        #if self.bounded and self.schedule != 'alwaysAll':
-        #    raise ValueError("Bounded size sketch should not be used for relative error.")
+        self.always = always
+        self.never = never
+        self.sectionSize = sectionSize
+        
+        self.initNumSections = 2 # an initial upper bound on log_2 of the number of compactions
 
-        #self.k = int(1/eps)+1
-        self.sectionSize = 2*(int(1/(4*eps))+1) # ensured to be even and positive (thus >= 2)
-        if self.schedule == 'alwaysHalf':
-            self.sectionSize = int(1/(eps**2))+1
+        # default setting of sectionSize, always, and never according to eps
+        if self.sectionSize == -1:
+            self.sectionSize = 2*(int(1/(4*eps))+1) # ensured to be even and positive (thus >= 2)
+        if self.always == -1:
+            self.always = self.sectionSize
 
-        self.numSections = 2 # an initial upper bound on log_2 (number of compactions)
+        self.neverGrows = False # if never is set by the user, then we do not let it grow
+        if self.never == -1:
+            self.never = self.sectionSize * self.initNumSections
+            self.neverGrows = True
 
         self.compactors = []
         self.H = 0
@@ -38,9 +44,8 @@ class RelativeErrorSketch:
         self.grow()
         
     def grow(self):
-        self.compactors.append(self.Compactor(schedule = self.schedule, sectionSize = self.sectionSize, numSections = self.numSections, height = self.H, alternate=self.alternate))
+        self.compactors.append(self.Compactor(schedule=self.schedule, sectionSize=self.sectionSize, numSections=self.initNumSections, always=self.always, never=self.never, neverGrows=self.neverGrows, height=self.H, alternate=self.alternate))
         self.H = len(self.compactors)
-        #if not bounded:
         self.updateMaxSize()
 
     def updateMaxSize(self):
@@ -62,9 +67,9 @@ class RelativeErrorSketch:
                 if h+1 >= self.H: self.grow()
                 self.compactors[h+1].extend(self.compactors[h].compact())
                 self.size = sum(len(c) for c in self.compactors)
-                if(self.lazy):
+                if(self.lazy and self.size < self.maxSize):
                     break
-        print(f"compression done: size {self.size}\t maxSize {self.maxSize}")
+        debugPrint(f"compression done: size {self.size}\t maxSize {self.maxSize}")
 
     def merge(self, other):
         # Grow until self has at least as many compactors as other
@@ -121,29 +126,25 @@ class RelativeCompactor(list):
         #self.eps = kwargs['eps']
         self.alternate = kwargs.get('alternate', True)
         self.sectionSize = kwargs.get('sectionSize', 32)
-        self.numSections = kwargs.get('numSections', 5)
-        self.height = kwargs.get('height', 0) # not used now
+        self.numSections = kwargs.get('numSections', 2)
+        self.always = kwargs.get('always', self.sectionSize)
+        self.never = kwargs.get('never', self.sectionSize * self.numSections)
+        self.neverGrows = kwargs.get('neverGrows', True)
+        self.height = kwargs.get('height', 0) 
         self.schedule = kwargs.get('schedule', "deterministic")
-        self.schedules = ['deterministic','randomized','alwaysHalf','alwaysAll']
+        self.schedules = ['deterministic','randomized']
         assert(self.schedule in self.schedules)
 
     def compact(self):
         assert(len(self) >= self.capacity())
-
-        if (self.numCompaction%2==1 and self.alternate):
-            self.offset = 1 - self.offset
-        else:
-            self.offset = int(random() < 0.5)
-
+        
         self.sort()
         
-        lastItem = None
-        
-        s = 0 # where the compaction starts; default is 0 for self.schedule == 'alwaysAll'
+        s = self.never # where the compaction starts; default is self.never
         secsToCompact = 0
 
         # choose a part to compact according to the selected schedule
-        if self.schedule == 'deterministic' or self.schedule == 'randomized':
+        if self.sectionSize > 0:
             if self.schedule == 'randomized':
                 while True:
                     secsToCompact = geometric(0.5)
@@ -151,31 +152,40 @@ class RelativeCompactor(list):
                         break
             else: #if self.schedule == 'deterministic' 
                 secsToCompact = trailing_zeros(self.numCompaction)
-            s = int(2/3 * self.capacity()) - secsToCompact * self.sectionSize # 1/3 of capacity always compacted
+            s = self.never + (self.numSections - secsToCompact) * self.sectionSize
                         
             # make the number of sections larger 
-            if self.numCompaction > 2**self.numSections:
-                self.numSections *= 2 # basically, doubling strategy on log_2 (number of compactions)
+            if self.numCompaction > 2 * 2**self.numSections: #TODO 4 --> sth else?
+                self.numSections *= 2 # basically, a doubling strategy on log_2 (number of compactions)
+                if self.neverGrows:
+                    self.never = self.sectionSize * self.numSections
             
-        elif self.schedule == 'alwaysHalf': 
-            s = int(self.capacity() / 2)
-        #TODO randomizedSimple: set s uniformly and randomly in [0.25 * capacity(), 0.75 * capacity()], or sth like that
+        #TODO schedule randomizedSimple: set s uniformly and randomly in [0.25 * capacity(), 0.75 * capacity()], or sth like that
         
+        if (len(self) - s)%2==1: # ensure that the compacted part has an even size
+            if s > 0: s -= 1
+            else: s += 1
+
         assert(s < len(self) - 1)
         
-        if ((len(self) - s)%2==1):
-            s += 1
+        # random offset for choosing odd/even items in the compacted part; if alternate, then random choice done every other time
+        if (self.numCompaction%2==1 and self.alternate):
+            self.offset = 1 - self.offset
+        else:
+            self.offset = int(random() < 0.5)
 
         for i in range(s+self.offset, len(self), 2):
-            yield self[i]
-        print(f"compacting {s}:\t secsToComp {secsToCompact}\t height {self.height}\t capacity {self.capacity()}\t size {len(self)}\t secSize {self.sectionSize}\t numSecs {self.numSections}")
-        self[s:] = []
-        print(f"compaction done: size {len(self)}")
+            yield self[i] # yield selected items
+        debugPrint(f"compacting {s}:\t secsToComp {secsToCompact}\t height {self.height}\t capacity {self.capacity()}\t size {len(self)}\t secSize {self.sectionSize}\t numSecs {self.numSections}")
+        self[s:] = [] # delete items that are not selected
+        debugPrint(f"compaction done: size {len(self)}")
 
         self.numCompaction += 1
 
     def capacity(self):
-        return 3 * self.numSections * self.sectionSize
+        cap = self.never + self.numSections * self.sectionSize + self.always
+        assert(cap > 1)
+        return cap
 
     def rank(self, value):
         return sum(1 for v in self if v <= value)
@@ -185,30 +195,77 @@ def trailing_zeros(n):
     s = str(n)
     return len(s)-len(s.rstrip('0'))
 
+def debugPrint(s):
+    if debug:
+        print(s)
+
+debug = False
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-eps', type=float, default=0.01,
-                        help='''controls the accuracy of the sketch which is, default is 0.01''')
+                        help='controls the accuracy of the sketch which is, default is 0.01; alternatively, accuracy can be controlled by -sec, -never, and -always')
     parser.add_argument('-t', type=str, choices=["string", "int", "float"], default='int',
                         help='defines the type of stream items, default="int".')
-    #parser.add_argument('-err', type=str, choices=["additive", "relative"], default='additive', # PV: SUPERSEDED BY THE FOLLOWING ARGUMENT
+    #parser.add_argument('-err', type=str, choices=["additive", "relative"], default='additive', # PV: SUPERSEDED BY THE FOLLOWING ARGUMENTS -- use -never=0, sectionSize=0, and always=k, for some k, for the additive error
     #                    help='sets the compactor to be either additive or relative, default="additive".')
-    parser.add_argument('-sch', type=str, choices=["deterministic", "randomized", "alwaysHalf", "alwaysAll"], default='deterministic',
-                        help='sets the schedule of compactions on each level to either deterministic or randomized; "alwaysHalf" is for compacting half of the buffer each time; use "alwaysAll" for additive error; default="deterministic".')
-    #parser.add_argument('-split', type=str, choices=["halves", "thirds"], default='thirds',
-    #                    help='an argument for the schedule, default="string".') #TODO explain & implement
+    parser.add_argument('-sch', type=str, choices=["deterministic", "randomized"], default='deterministic',
+                        help='sets the schedule of compactions on each level to either deterministic or randomized; default="deterministic".')
+    parser.add_argument('-sec', type=int, default=-1,
+                        help='size of each buffer section, should be even; by default set according to -eps.')
+    parser.add_argument('-never', type=int, default=-1,
+                        help='size of the buffer part that is never compacted, by default set to the section size times the number of sections.')
+    parser.add_argument('-always', type=int, default=-1,
+                        help='size of the buffer part that is always compacted, by default set to the section size.')
+    parser.add_argument('-debug', type=bool, default=False,
+                        help='print debug messages; default=False.')
     args = parser.parse_args()
     
+    debug = args.debug
     eps = args.eps
+    type = args.t
     conversions = {'int':int, 'string':str, 'float':float}
          
-    sketch = RelativeErrorSketch(eps=eps, schedule=args.sch)
+    sketch = RelativeErrorSketch(eps=eps, schedule=args.sch, always=args.always, never=args.never, sectionSize=args.sec)
+    items = []
     for line in sys.stdin:
-        item = conversions[args.t](line.strip('\n\r'))
+        item = conversions[type](line.strip('\n\r'))
         sketch.update(item)
-        
-    for (item, quantile) in sketch.cdf():
-        print(f"{quantile}\t{item}")
-        
-    print(f"final size {sketch.size}\t maxSize {sketch.maxSize}")
+        items.append(item)
+     
+    #cdf = sketch.cdf()
+    #if args.cdf==True:
+    #    for (item, quantile) in cdf:
+    #        print(f"{quantile}\t{item}")
+    
+    # calculate maximum relative error
+    ranks = sketch.ranks()
+    items.sort()
+    n = len(items)
+    maxErrStored = 0
+    print("item|apx.r.|true r.|err")
+    # maximum relative error just among stored items
+    for i in range(0, len(ranks)):
+        (item, rank) = ranks[i]
+        trueRank = items.index(item) + 1
+        err = abs(trueRank - rank) / trueRank
+        maxErrStored = max(maxErrStored, err)
+        print(f"{item}\t{rank}\t{trueRank}\t{err}")
+
+    # maximum relative error just among all items
+    print("\n ************************************")
+    maxErr = 0
+    i = 1
+    j = 0
+    for item in items:
+        if j < len(ranks) - 1 and item == ranks[j+1][0]:
+            j += 1
+        (stored, rank) = ranks[j]
+        err = abs(rank - i) / i
+        maxErr = max(maxErr, err)
+        #print(f"item {item}\t stored {stored}\t rank {rank}\t trueRank {i}\t{err}")
+        i += 1
+
+
+    print(f"n={n}\nmax rel. error {maxErr}\nmax rel. error of stored {maxErrStored}\nfinal size {sketch.size}\nmaxSize {sketch.maxSize}")
