84507c44e39a1b3b6866fff6f0f834970b32f41b
Pavel Vesely <vesely@iuuk.mff.cuni.cz>
2020-08-12 20:13:11 +0200

    params for order adv

--- streamMaker.py
+++ streamMaker.py
@@ -9,9 +9,9 @@ from math import sqrt,ceil
 
 class StreamMaker():
     def __init__(self):
-        self.orders = ['sorted','reversed','zoomin','zoomout','sqrt','random','adv1000'] 
+        self.orders = ['sorted','reversed','zoomin','zoomout','sqrt','random','adv'] 
         
-    def make(self, n, order=''):
+    def make(self, n, order='', p=1000, g=0, s=1):
         assert(order in self.orders)
         
         if order == 'sorted': # sorted order
@@ -42,13 +42,13 @@ class StreamMaker():
                     skip+=1 
                 initialSkip+=1
                 initialItem+=initialSkip
-        elif order == 'adv1000': 
-            t = 1000
+        elif order == 'adv': 
+            t = p
             m = ceil(n/t)
             for i in range(t):
-                for j in range(t + m*(t-i), t + m*(t-i-1), -1):
+                for j in range(g + t + m*(t-i), g + t + m*(t-i-1), -1):
                     yield j
-                yield i
+                yield s*i
         else: # order == 'random':
             items = list(range(n))
             random.shuffle(items)
@@ -62,11 +62,17 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', type=int, help='the number of generated elements', default=1000)
+    parser.add_argument('-p', type=int, help='parameter for generating some orders (currently for order adv only)', default=1000)
+    parser.add_argument('-g', type=int, help='another parameter for generating some orders (currently for order adv only)', default=0)
+    parser.add_argument('-s', type=int, help='yet another parameter for generating some orders (currently for order adv only)', default=1)
     parser.add_argument('-o', type=str, help='the order of the streamed integers.',
                         choices=streamer.orders)
     args = parser.parse_args()
     
     n = args.n if args.n > 0 else 1000
     order = args.o if args.o in streamer.orders else 'random'
-    for item in streamer.make(n, order):
+    p = args.p if args.p > 0 else 1000
+    g = args.g if args.g > 0 else 0
+    s = args.s if args.s > 0 else 1
+    for item in streamer.make(n, order, p, g, s):
         sys.stdout.write('%d\n'%item)
